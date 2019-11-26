38967a05f14fcd3ce9df77429dc3570b66fe532d
fiotto <fiotto42@gmail.com>
2019-11-26 23:23:00 +0900

    update stulelint.config.js

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,4 +1,13 @@
 module.exports = {
-  // add your custom config here
-  // https://stylelint.io/user-guide/configuration
+  rules: {
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['extends', 'ignores']
+      }
+    ],
+    indentation: 2,
+    'number-leading-zero': null,
+    'unit-whitelist': ['px', 'deg', 'em', 'rem', 's']
+  }
 }
