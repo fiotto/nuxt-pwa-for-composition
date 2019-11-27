6c840a914dff5f4a3f9f3f8cc921e6744fd875b0
fiotto <fiotto42@gmail.com>
2019-11-27 21:41:24 +0900

    add typescript modules

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,7 +39,8 @@ export default {
     '@nuxtjs/eslint-module',
     // Doc: https://github.com/nuxt-community/stylelint-module
     '@nuxtjs/stylelint-module',
-    '@nuxtjs/vuetify'
+    '@nuxtjs/vuetify',
+    '@nuxt/typescript-build'
   ],
   /*
    ** Nuxt.js modules
@@ -84,6 +85,6 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {}
+    extend() {}
   }
 }
